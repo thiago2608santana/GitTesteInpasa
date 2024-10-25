1af7d814cb1a8ed6c29a565841267d5091942050
thiago2608santana <thiago2608santana@gmail.com>
2024-10-25 16:25:51 -0400

    Create Funcoes.py
    Primeira versão

---
+++ Funcoes.py
@@ -0,0 +1,34 @@
+def calcular_inss(salario_bruto):
+
+    if salario_bruto <= 1412:
+        inss = salario_bruto*0.075
+    elif salario_bruto > 1412 and salario_bruto <= 2666.68:
+        inss = salario_bruto*0.09
+        inss = inss - 21.18
+    elif salario_bruto > 2666.68 and salario_bruto <= 4000.03:
+        inss = salario_bruto*0.12
+        inss = inss - 101.18
+    else:
+        inss = salario_bruto*0.14
+        inss = inss - 181.18
+
+    return round(inss,2)
+
+def calcular_ir(salario_bruto):
+
+    inss = calcular_inss(salario_bruto)
+
+    salario_descontado = salario_bruto - inss
+    
+    if salario_descontado <= 2259.20:
+        ir = 0
+    elif salario_descontado > 2259.20 and salario_descontado <= 2826.65:
+        ir = salario_descontado*0.075 - 169.44
+    elif salario_descontado > 2826.65 and salario_descontado <= 3751.05:
+        ir = salario_descontado*0.15 - 381.44
+    elif salario_descontado > 3751.05 and salario_descontado <= 4664.68:
+        ir = salario_descontado*0.225 - 662.77
+    else:
+        ir = salario_descontado*0.275 - 896.00
+    
+    return round(ir,2)
